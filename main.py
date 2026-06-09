a915c31156bb6a2453a3d2d059165e4bc74cb04e
IRadical <m4ouyea@gmail.com>
2026-06-09 13:39:08 -0600

    feat: blueprint and dynamic routes structure

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 import json
 from pathlib import Path
 from flask import Flask, render_template
+from app.routes.main_routes import main_bp
 
 ruta_config = Path(__file__).parent / "config" / "config.json"
 with open(ruta_config, "r") as f:
     config = json.load(f)
 
 app = Flask(__name__)
-@app.route("/")
-def index():
-    return render_template("index.html",
-                           nombre_proyecto=config["configuracion"]["nombre_proyecto"],)
+app.register_blueprint(main_bp)
 
 if __name__ == "__main__":
     puerto = config["configuracion"]["pto_flask"]
